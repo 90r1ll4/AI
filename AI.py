03c419dc8c16f1c5b22ff86627e82a99a7cca425
HACKAI <45795129+HAc-KAI@users.noreply.github.com>
2020-10-27 17:46:50 +0530

    Update AI.py
    wiki[pedia

--- AI.py
+++ AI.py
@@ -1,9 +1,10 @@
 import pyttsx3
 import datetime
 import speech_recognition as sr
-engine = pyttsx3.init()
+import wikipedia
+engine = pyttsx3.init('espeak')
 voices = engine.getProperty('voices')
-engine.setProperty('voices',voices[0].id)
+engine.setProperty('voices',voices[1].id)
 #print(voices)
 #engine.say("hello")
 #engine.runAndWait()
@@ -16,7 +17,7 @@ def wishme():
         speak("Good Afternoon Sir,Lets get to work")
     elif hour>17:
         speak("Good evening Sir,long day uhh")
-    speak("I am Alen, I am ready to roll")
+    speak("I am Alen, how may i help")
 
 
 
@@ -29,19 +30,36 @@ def take():
     r=sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        r.pause_threshold = 0.8
         audio=r.listen(source)
     try:
         print("Recognizing...")
-        query=r.recognize_google(audio, language='en-in')
+        query=r.recognize_google(audio)
         print(f"Boss said: {query}\n")  
     except Exception as e:
-        print(e)
+       # print(e)
         print("Couldn't Hear You..")
         return "None"        
     return query
 
 if __name__ == "__main__":
+    engine.setProperty('rate', 150)
     #speak("Harry is a good boy")
     wishme()
-    take()
+    while True:
+       query=take().lower()
+       #logics
+       if 'wikipedia' in query:
+          speak('Searching in wikipedia')
+          query = query.replace("wikipedia", " ")
+          try: 
+             results = wikipedia.summary(query,sentences=2)
+          except Exception as e: 
+              print("Server is slow, Please try again")
+              speak("Server is slow, Please try again")
+                #print(engine.getProperty('rate'))
+          speak("According to wikipedia")
+          print(results)
+          speak(results)
+          
